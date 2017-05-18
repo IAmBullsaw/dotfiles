27cfca6c97245b820c9dc578581df0591ea9ad6e
Oskar Jansson <oskja067@student.liu.se>
2017-05-18 21:35:08 +0200

    adds: small script now links .spacemacs

--- link.sh
+++ link.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 SPACEMACS=~/.spacemacs
+printf "[.spacemacs] "
 if [ ! -f $SPACEMACS ]; then
-    echo "linking .spacemacs ..."
+    printf "linking. "
+    ln -s "`pwd`/.spacemacs" ~/.spacemacs
+    printf "OK\n"
+else
+    printf "OK\n"
 fi
-pwd
