f0d953560e4a50b14c9bca4305bee8e084be8c89
bullen <bullen@konserver.lol>
2017-05-18 22:29:41 +0200

    adds: asks for user input which editor is chosen

--- link.sh
+++ link.sh
@@ -1,10 +1,26 @@
 #/bin/bash
 SPACEMACS=~/.spacemacs
-printf "[.spacemacs] "
-if [ ! -f $SPACEMACS ]; then
-    printf "linking. "
-    ln -s "`pwd`/.spacemacs" ~/.spacemacs
-    printf "OK\n"
+EMACS=~/.emacs.d/init.el
+
+printf "Choose your editor:\n\n"
+read -n 1 -p "Emacs, Spacemacs or none? (e/s/*): " answer
+printf "\n"
+if [ "$answer" == "s" ]; then
+    printf "[.spacemacs] "
+    if [ ! -f $SPACEMACS ]; then
+        printf "linking. "
+        ln -s "`pwd`/.spacemacs" $SPACEMACS
+    else
+        printf "OK\n"
+    fi
+elif [ "$answer" == "e" ]; then
+    printf "[.emacs] "
+    if [ ! -f $EMACS ]; then
+        printf "linking. "
+        ln -s "`pwd`/init.el" $EMACS
+    else
+        printf "OK\n"
+    fi
 else
-    printf "OK\n"
+    printf "No editor chosen.\n"
 fi
