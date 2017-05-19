f8358807692a5ea7fc1b92ceffaf11192c529ade
bullen <bullen@konserver.lol>
2017-05-19 09:35:01 +0200

    adds: questions to init.el as well

--- link.sh
+++ link.sh
@@ -6,26 +6,37 @@ printf "Choose your editor:\n\n"
 read -n 1 -p "Emacs, Spacemacs or none? (e/s/*): " answer
 printf "\n"
 if [ "$answer" == "s" ]; then
-    printf "[.spacemacs] "
+    PRE=.spacemacs
     if [ ! -f $SPACEMACS ]; then
-        printf "linking. "
+        printf "[$PRE] linking.\n"
         ln -s "`pwd`/.spacemacs" $SPACEMACS
     else
-        read -n 1 -p "Want to remove old config? (y/n):" answer
+        read -n 1 -p "[$PRE] Want to remove old config? (y/n):" answer
+        printf "\n"
         if [ "$answer" == y ]; then
             rm $SPACEMACS
-            printf "linking. "
+            printf "[$PRE] removed config\n"
+            printf "[$PRE] linking. \n"
             ln -s "`pwd`/.spacemacs" $SPACEMACS
         fi
     fi
+    printf "[$PRE] done\n"
 elif [ "$answer" == "e" ]; then
-    printf "[.emacs] "
+    PRE=init.el
     if [ ! -f $EMACS ]; then
-        printf "linking. "
+        printf "[$PRE] linking.\n"
         ln -s "`pwd`/init.el" $EMACS
     else
-        printf "OK\n"
+        read -n 1 -p "[$PRE] Want to remove old config? (y/n):" answer
+        printf "\n"
+        if [ "$answer" == y ]; then
+            rm $EMACS
+            printf "[$PRE] removed config\n"
+            printf "[$PRE] linking. \n"
+            ln -s "`pwd`/init.el" $EMACS
+        fi
     fi
+    printf "[$PRE] done\n"
 else
     printf "No editor chosen.\n"
 fi
